6a8babc28656202bf79c9f082d957ed415f94b0c
Phu Koi Ka <devkoika@gmail.com>
2024-05-07 10:09:33 +0700

    test

--- chat.js
+++ chat.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log(window.parent.document.getElementById("wi"));
+document.addEventListener("DOMContentLoaded", (e) => {
+  var dom = {
+    frame: window.parent.document.getElementById("widget-frame"),
+    btn: document.getElementById("send"),
+  };
+
+  btn.addEventListener("click", () => {
+    if (dom.frame) {
+      if (state === 1) {
+        dom.frame.style.width = "75px";
+        dom.frame.style.height = "75px";
+      } else {
+        dom.frame.style.height = "480px";
+        dom.popupContainer.style.height = "370px";
+        dom.popupBody.style.height = "280px";
+      }
+    }
+  });
 });
